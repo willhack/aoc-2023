702e6d31710ad6efee27e477e1ab900bc153eb5c
Will Hack <18036720+willhack@users.noreply.github.com>
2023-12-04 10:24:30 -0500

    chore: add valueAdder util

--- utils.ts
+++ utils.ts
@@ -9,3 +9,6 @@ export const nummable = (char: string) => !Number.isNaN(+char);
 
 export const truthyCounter = <T>(arr: T[], booleanFn: (args: T) => boolean) =>
   arr.reduce((acc, cur) => (booleanFn(cur) ? acc + 1 : acc), 0);
+
+export const valueAdder = (obj: Record<string, number>) =>
+  Object.values(obj).reduce((a, b) => a + b, 0);
