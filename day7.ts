28a8aa7bba09f75ae9feaf6ddb886711a5e23cc7
Will Hack <18036720+willhack@users.noreply.github.com>
2023-12-07 21:29:05 -0500

    feat: finish part2 and refactor

--- day7.ts
+++ day7.ts
@@ -2,6 +2,14 @@ import { getInput } from "./utils";
 
 const input = await getInput(7);
 
+// const sample = [
+//   "32T3K 765",
+//   "T55J5 684",
+//   "KK677 28",
+//   "KTJJT 220",
+//   "QQQJA 483",
+// ];
+
 const strengthMap: Record<string, number> = {
   A: 14,
   K: 13,
@@ -19,77 +27,61 @@ const strengthMap: Record<string, number> = {
 };
 
 /*
-Five of a kind: AAAAA - 1 kind
-Four of a kind: AA8AA - 2 kinds - max 4
-Full house: 23332 - 2 kinds - max 3
-Three of a kind: TTT98 - 3 kinds - max 3
-Two pair: 23432 - 3 kinds - max 2
-One pair: A23A4 - 4 kinds
-High card: 23456 - 5 kinds
+Five of a kind  --> 7
+Four of a kind  --> 6
+Full house      --> 5
+Three of a kind --> 4
+Two pair        --> 3
+One pair        --> 2
+High card       --> 1
  */
-function calculateStrength(countedCardValues: number[]) {
-  const uniqueCardCount = countedCardValues.length;
-  if (uniqueCardCount === 1) return 7;
-  if (uniqueCardCount === 2) {
-    return Math.max(...countedCardValues) === 4 ? 6 : 5;
-  }
-  if (uniqueCardCount === 3) {
-    return Math.max(...countedCardValues) === 3 ? 4 : 3;
-  }
-  if (uniqueCardCount === 4) return 2;
+function calculateStrength(countedCardValues: number[], jokers = 0) {
+  const max = Math.max(...countedCardValues, 0) + jokers;
+  if (max === 5) return 7;
+  if (max === 4) return 6;
+  if (max === 3)
+    return (!jokers && countedCardValues.includes(2)) ||
+      countedCardValues.filter((n) => n === 2).length === 2
+      ? 5
+      : 4;
+  if (max === 2) return jokers || countedCardValues.filter((n) => n === 2).length === 1 ? 2 : 3;
   return 1;
 }
 
-const sample = [
-  // "32T3K 765",
-  // "T55J5 684",
-  // "KK677 28",
-  // "KTJJT 220",
-  // "QQQJA 483",
-  "AAAAA 1",
-  "AA8AA 1",
-  "23332 1",
-  "TTT98 1",
-  "23432 1",
-  "A23A4 1",
-  "23456 1",
-  "KJKJK 1",
-  "JKJKJ 1",
-];
-
-function parseInput(input: string[]) {
-  return input.map((h) => {
-    const [unsortedHand, rawBid] = h.split(" ");
-    const cardCounter = unsortedHand.split("").reduce((acc: Record<string, number>, cur) => {
+const parseInput = (input: string[]) =>
+  input.map((h) => {
+    const [hand, rawBid] = h.split(" ");
+    const cardCounter = hand.split("").reduce((acc: Record<string, number>, cur) => {
       acc[cur] = ++acc[cur] || 1;
       return acc;
     }, {});
 
-    const strength = calculateStrength(Object.values(cardCounter));
-
-    // well we don't need THIS logic...not real poker
-    // let hand = "";
-    // while (Object.values(cardCounter).length) {
-    //   let max = { key: "", count: 0 };
-    //   for (const [card, num] of Object.entries(cardCounter)) {
-    //     if (num > max.count || (num === max.count && strengthMap[card] > strengthMap[max.key])) {
-    //       max = { key: card, count: num };
-    //     }
-    //   }
-    //   hand += max.key.repeat(max.count);
-    //   delete cardCounter[max.key];
-    // }
-
     return {
-      hand: unsortedHand,
+      hand,
       bid: +rawBid,
-      strength,
+      cardCounter,
     };
   });
-}
 
-type Hand = ReturnType<typeof parseInput>[number];
-function compareHands(a: Hand, b: Hand) {
+const part1Transform = (parsedInput: ReturnType<typeof parseInput>) =>
+  parsedInput.map((hand) => ({
+    ...hand,
+    strength: calculateStrength(Object.values(hand.cardCounter)),
+  }));
+
+const part2Transform = (parsedInput: ReturnType<typeof parseInput>) =>
+  parsedInput.map((hand) => {
+    const jokers = hand.cardCounter.J ?? 0;
+    delete hand.cardCounter.J;
+    return {
+      ...hand,
+      strength: calculateStrength(Object.values(hand.cardCounter), jokers),
+    };
+  });
+
+type Transformer = typeof part1Transform | typeof part2Transform;
+
+function compareHands<T extends ReturnType<Transformer>[number]>(a: T, b: T) {
   if (a.strength === b.strength) {
     for (let i = 0; i < 5; i++) {
       if (a.hand[i] === b.hand[i]) continue;
@@ -99,12 +91,11 @@ function compareHands(a: Hand, b: Hand) {
   return a.strength - b.strength;
 }
 
-const totalWinnings = parseInput(input)
-  .sort(compareHands)
-  // .map((h) => h.hand);
-  .reduce((acc, cur, i) => acc + cur.bid * (i + 1), 0);
+const totalWinnings = (fn: Transformer) =>
+  fn(parseInput(input))
+    .sort(compareHands)
+    .reduce((acc, cur, i) => acc + cur.bid * (i + 1), 0);
 
-console.log(totalWinnings);
-
-// sort based on hand strength
-// profit
+console.log(totalWinnings(part1Transform));
+strengthMap.J = 1;
+console.log(totalWinnings(part2Transform));
