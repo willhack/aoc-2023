3440011a7ce03c89a59f1fdaa1210d8d5ca7ea23
Will Hack <18036720+willhack@users.noreply.github.com>
2023-12-08 11:08:52 -0500

    feat: finish day8 with lcm

--- day8.ts
+++ day8.ts
@@ -1,33 +1,51 @@
-import { getInput } from "./utils";
+import { getInput, lcm } from "./utils";
 
 const input = await getInput(8, "\n\n");
 
-const sample = `
-LLR
+// const sample = `
+// LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
-`;
-
-const nodeRegex = /([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)/;
+// 11A = (11B, XXX)
+// 11B = (XXX, 11Z)
+// 11Z = (11B, XXX)
+// 22A = (22B, XXX)
+// 22B = (22C, 22C)
+// 22C = (22Z, 22Z)
+// 22Z = (22B, 22B)
+// XXX = (XXX, XXX)
+// `.split("\n\n");
 
 const [directions, nodes] = input.map((l) => l.trim());
 
+const nodeRegex = /(\w{3}) = \((\w{3}), (\w{3})\)/;
 const nodeMap = nodes.split("\n").reduce((map: Record<string, Record<string, string>>, node) => {
   const [, K, L, R] = nodeRegex.exec(node) ?? [];
-  map[K] = { L: L, R: R };
+  map[K] = { L, R };
   return map;
 }, {});
 
-let count = 0;
-let cur = "AAA";
-let i = 0;
+const pathCounter =
+  (conditionalFn: (curArg: string) => boolean) =>
+  (init = "AAA") => {
+    let count = 0;
+    let cur = init;
+    let i = 0;
 
-while (cur !== "ZZZ") {
-  count++;
-  cur = nodeMap[cur][directions[i]];
-  if (++i === directions.length) i = 0;
+    while (!conditionalFn(cur)) {
+      count++;
+      cur = nodeMap[cur][directions[i]];
+      if (++i === directions.length) i = 0;
+    }
+    return count;
+  };
+
+const part1 = pathCounter((n) => n === "ZZZ")();
+
+function part2() {
+  const resultsList = Object.keys(nodeMap)
+    .filter((n) => n.endsWith("A"))
+    .map((n) => pathCounter((c: string) => c.endsWith("Z"))(n));
+  return lcm(resultsList);
 }
 
-console.log(count);
+console.log({ part1, part2: part2() }); // 1: 17287, 2: 18625484023687
--- utils.ts
+++ utils.ts
@@ -12,3 +12,7 @@ export const truthyCounter = <T>(arr: T[], booleanFn: (args: T) => boolean) =>
 
 export const valueAdder = (obj: Record<string, number>) =>
   Object.values(obj).reduce((a, b) => a + b);
+
+export const gcd = (a: number, b: number): number => (b == 0 ? a : gcd(b, a % b));
+
+export const lcm = (arr: number[]) => arr.reduce((lcm, cur) => (cur * lcm) / gcd(cur, lcm));
